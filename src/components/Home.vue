cf750a5ea79e131f6d1389e3cb942a76eb8d712c
Guikingone <guillaume.loulier@hotmail.fr>
2017-04-27 19:29:44 +0200

    [FIX](CI + Router)[!M] CI|Corrected on deployment + Router|Corrected

---
+++ src/components/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="hello">
+    <p>Because creating the future require expertise !</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  p {
+    text-align: center;
+    font-family: "Agency FB";
+    font-size: 40px;
+  }
+</style>
